cfae49921b1eb63e13f6e1d18d4a911f73402d79
Yoshid01 <garlic.ba.0129@gmail.com>
2017-10-17 00:21:30 +0900

    Add code to post slack webhook by pressing switch

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nefry-bt-samples
+
+### slack-webhook
+
+* Setting environment variables
+    * For `WebhookURL` , set the path part of url of your slack webhook .
+    * For `UserName` , set the display name to post in slack webhook .
+    * For `IconEmoji` , set the name of the emoji of the display icon .
+    * For `Text1~3` , specify three texts to post on slack . Then , one random body will be posted .
+
+* How to use
+    * Press the switch labeled "SW" on the Nefry BT .
---
+++ slack-webhook/slack-webhook.ino
@@ -0,0 +1,63 @@
+#include <Nefry.h>
+#include <WiFiClientSecure.h>
+
+const int httpPort = 443;
+const char* host = "hooks.slack.com";
+
+void setup() {
+  Nefry.setStoreTitle("WebhookURL", 0);
+  Nefry.setStoreTitle("UserName", 1);
+  Nefry.setStoreTitle("IconEmoji", 2);
+  Nefry.setStoreTitle("Text1", 3);
+  Nefry.setStoreTitle("Text2", 4);
+  Nefry.setStoreTitle("Text3", 5);
+  Nefry.enableSW();
+  Nefry.setLed(0, 0, 255);
+}
+
+void loop() {
+  if(Nefry.readSW()) {
+    postIncommingWebHook();
+    Nefry.setLed(0, 0, 255);
+  }
+}
+
+void postIncommingWebHook() {
+
+  String url = Nefry.getStoreStr(0);
+  String userName = Nefry.getConfStr(1);
+  String iconEmoji = Nefry.getConfStr(2);
+  String* texts = new String[3];
+  texts[0] = Nefry.getConfStr(3);
+  texts[1] = Nefry.getConfStr(4);
+  texts[2] = Nefry.getConfStr(5);
+
+  WiFiClientSecure client;
+
+  if (client.connect(host, httpPort)) {
+
+    String payload = String("payload={") +
+                            "'text': '" + texts[random(3)] + "', " +
+                            "'username': '" + userName + "', " +
+                            "'icon_emoji': '" + iconEmoji + "', " +
+                            "}";
+
+    client.print(
+                 String("POST ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + String(host) + "\r\n" +
+                 "Connection: close\r\n"
+                 "Content-Type: application/x-www-form-urlencoded\r\n" +
+                 "Content-Length: " + String(payload.length()) + "\r\n" + 
+                 "\r\n" +
+                 payload + "\r\n"
+                 );
+
+    Nefry.setLed(0, 255, 0);
+    Nefry.ndelay(1000);
+
+  } else {
+    Nefry.setLed(255, 0, 0);
+    Nefry.ndelay(1000);
+  }
+}
+
